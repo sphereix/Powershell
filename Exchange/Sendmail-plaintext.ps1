6bb1fe4b79a75d8fa5dc83c24a28f1742347f22c
sphereix <45791069+sphereix@users.noreply.github.com>
2018-12-11 13:59:56 +0000

    Update Sendmail-plaintext.ps1

--- Exchange/Sendmail-plaintext.ps1
+++ Exchange/Sendmail-plaintext.ps1
@@ -1,18 +1,18 @@
-$Username = "tom.hughes@morsesclub.com";
-$Password = "R3l8able#!";
+$Username = "tom.hughes@company.com";
+$Password = "blahblahblahetc";
 $path = "C:\attachment.txt";
 
 function Send-ToEmail([string]$email, [string]$attachmentpath){
 
     $message = new-object Net.Mail.MailMessage;
-    $message.From = "alerts@morsesclub.com";
+    $message.From = "alerts@company.com";
     $message.To.Add($email);
     $message.Subject = "subject text here...";
     $message.Body = "body text here...";
     $attachment = New-Object Net.Mail.Attachment($attachmentpath);
     $message.Attachments.Add($attachment);
 
-    $smtp = new-object Net.Mail.SmtpClient("cas.morsesclub.local", "587");
+    $smtp = new-object Net.Mail.SmtpClient("cas.company.local", "587");
     $smtp.EnableSSL = $true;
     $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
     $smtp.send($message);
