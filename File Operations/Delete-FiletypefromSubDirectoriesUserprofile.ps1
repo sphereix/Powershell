7f765023582a2339b733b0b6a924e48442949388
sphereix <45791069+sphereix@users.noreply.github.com>
2019-03-08 15:22:47 +0000

    Add files via upload
    good for deleting a file type from all user profiles (multiple sub directories)

---
+++ File Operations/Delete-FiletypefromSubDirectoriesUserprofile.ps1
@@ -0,0 +1,11 @@
+﻿$path = "C:\Users"
+$child_path = "\CTI\CORRPLAN\"
+$files_filter = "*.log"
+
+Get-ChildItem $path -Directory -Exclude Default*,Public | foreach {
+
+    $joined_path = Join-Path -Path $_.FullName -ChildPath $child_path
+    If (test-path $joined_path) {
+        Remove-Item "$joined_path\$files_filter" -Force
+    }
+}
