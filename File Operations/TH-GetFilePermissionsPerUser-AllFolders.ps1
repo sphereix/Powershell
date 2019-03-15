a7a662f0bcb06116fc8626e42235412bc2c589b4
sphereix <45791069+sphereix@users.noreply.github.com>
2019-03-15 17:54:04 +0000

    Create TH-GetFilePermissionsPerUser-AllFolders.ps1

---
+++ File Operations/TH-GetFilePermissionsPerUser-AllFolders.ps1
@@ -0,0 +1,24 @@
+###################################################
+#                                                 #
+#  Frontline 2019 - Thomas Hughes & Aaron Tulloch #
+#                                                 #
+###################################################
+
+Start-Transcript -path C:\temp\output5.txt -append
+$folderlist = "C:\temp\folderlist2.txt"
+$User = "first.surname"
+
+ForEach ($Folder in (Get-Content $Folderlist))
+{
+$permission = (Get-Acl $Folder -ErrorAction SilentlyContinue).Access | ?{$_.IdentityReference -match $User} | Select IdentityReference,FileSystemRights
+If ($permission){
+$permission | % {Write-Host "$user ^ $folder ^ Full Access"}
+}
+Else {
+Write-Host "$User ^ $Folder ^ No Access"
+} 
+
+}
+
+Stop-Transcript
+
