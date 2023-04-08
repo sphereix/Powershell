fa777f3e03852998fffa4c16d01ea7e5f2e64b3e
sphereix <45791069+sphereix@users.noreply.github.com>
2023-04-08 03:09:06 +0100

    Update query-user.ps1

--- query-user.ps1
+++ query-user.ps1
@@ -1,33 +1,37 @@
-# This script will read the list of servers from a .txt file, 
-# then export the groups that exist on the server 
-# then query active directory to see the users in those groups, including nested groups 
+# This script will read the list of servers from a .txt file, then export the groups that exist on the server then query active directory to see the users in those groups, including nested groups
 
-# Import the required modules 
-Import-Module csv 
-Import-Module activedirectory 
+# Import the required modules
+Import-Module csv
+Import-Module activedirectory
 
-# Get the path to the .txt file 
-$txtPath = "C:\Path\To\Servers.txt" 
+# Get the path to the .txt file
+$txtPath = "C:\Path\To\Servers.txt"
 
-# Read in the .txt file 
-$servers = Get-Content $txtPath 
+# Read in the .txt file
+$servers = Get-Content $txtPath
 
-# For each server in the .txt file 
-foreach ($server in $servers) { 
-    # Get the groups that exist on the server 
-    $groups = Get-ADGroup -Server $server -Recursive | Select-Object Name, DistinguishedName
+# For each server in the .txt file
+foreach ($server in $servers) {
 
-    # Export the groups to a text file 
-    Export-Csv "C:\Path\To\Groups_$($server).csv" -InputObject $groups -NoTypeInformation
+    # Get the groups that exist on the server
+    $groups = Get-ADGroup -Server $server -Recursive
 
-    # For each group on the server
-    foreach ($group in $groups) {
-        # Get the list of users in the group (including nested groups)
-        $users = Get-ADGroupMember -Identity $group.DistinguishedName -Recursive | Where-Object { $_.objectClass -eq 'user' } | Select-Object SamAccountName
+    # Export the groups to a text file
+    Export-Csv "C:\Path\To\Groups.csv" -InputObject $groups
 
-        # For each user in the group
-        foreach ($user in $users) {
-            # Write the user's name to the console 
+    # Query Active Directory to see the users in those groups
+    $users = Get-ADUser -Filter * -Properties MemberOf -ResultSetSize Unlimited
+
+    # For each user in Active Directory
+    foreach ($user in $users) {
+
+        # Check if the user is a member of any of the groups on the server
+        $isMember = $groups | ForEach-Object { $_ -Contains $user.SamAccountName }
+
+        # If the user is a member of any of the groups on the server
+        if ($isMember) {
+
+            # Write the user's name to the console
             Write-Host $user.SamAccountName
         }
     }
