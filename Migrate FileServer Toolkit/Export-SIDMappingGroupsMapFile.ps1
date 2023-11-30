0fbed632af4c2c9768929ffcb4d02f2244082ed4
sphereix <45791069+sphereix@users.noreply.github.com>
2023-11-30 15:09:41 +0000

    Delete Migrate FileServer Toolkit directory

--- Migrate FileServer Toolkit/Export-SIDMappingGroupsMapFile.ps1
+++
@@ -1,4 +0,0 @@
-﻿cd C:\SIDHistoryGroups
-Export-SIDMappingCustom -ObjectType User -Property samAccountName -OldCSV .\2SFG-Groups.csv -NewCSV .\domain1.org-Groups.csv -MapFile SIDMap.csv
-Get-Content .\SIDMap.csv
-Get-Content .\SIDMap.csv | Measure-Object
--- Migrate FileServer Toolkit/Export-SIDMappingUserMapFile.ps1
+++
@@ -1,7 +0,0 @@
-﻿#-MapFile is the output mapfile - this needs to be called SIDMap.csv for the Convert-SIDHistoryNTFS module to work - Convert-SIDHistoryNTFS is ran
-# from this directory when all 3 files are in place - see "Convert-SharePermissionsSIDS.ps1"
-
-cd C:\SIDHistoryUsers
-Export-SIDMappingCustom -ObjectType User -Property samAccountName -OldCSV .\2sfg-users.csv -NewCSV .\domain1.org-users.csv -MapFile SIDMap.csv
-Get-Content .\SIDMap.csv
-Get-Content .\SIDMap.csv | Measure-Object
